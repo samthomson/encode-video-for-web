65492f206de2d2fc772dc260950bde882f443d8d
samthomson <samt@samt.st>
2017-05-03 21:24:03 +0200

    convert all

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
-exports.convert = function(sFilePath) {
-  console.log(`convert ${sFilePath}`);
+
+var ffmpeg = require('fluent-ffmpeg');
+
+function pMP4 (sInputFile, sOutputFilename) {
+	return new Promise(
+	    (resolve, reject) => {
+	        var startMP4 = new Date();
+	        ffmpeg(sInputFile)
+	            .output(`${sOutputFilename}.mp4`)
+	            .on('end', function() {
+	                resolve();
+	            })
+	            .run();
+	    }
+	);
+}
+
+function pWEBM (sInputFile, sOutputFilename) {
+	return new Promise(
+		(resolve, reject) => {
+	        var startWEBM = new Date();
+	        ffmpeg(sInputFile)
+	            .output(`${sOutputFilename}.webm`)
+	            .on('end', function() {
+	                resolve();
+	            })
+	            .run();
+	    }
+	);
+}
+
+function pAVI (sInputFile, sOutputFilename) {
+	return new Promise(
+		(resolve, reject) => {
+	        var startAVI = new Date();
+	        ffmpeg(sInputFile)
+	            .output(`${sOutputFilename}.avi`)
+	            .on('end', function() {
+	                resolve();
+	            })
+	            .run();
+	    }
+	);
+}
+
+function convertAll(sInputFile, sOutputFilename) {
+	Promise.all(
+		[
+			pMP4(sInputFile, sOutputFilename),
+			pWEBM(sInputFile, sOutputFilename),
+			pAVI(sInputFile, sOutputFilename)
+		]
+	).then(() => {
+	    console.log('finished conevrting')
+	});
+}
+
+exports.encode = function(sInputFile, sOutputFilename, aOutputFormats, iKilobytesPerSecond) {
+	console.log(`convert ${sInputFile} into ${sOutputFilename}`);
+	// pMP4(sInputFile, sOutputFilename)
+	convertAll(sInputFile, sOutputFilename)
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,18 @@
 # adaptive-encoding
 
+video from different cameras will be of a different bit rate, so applying the same converstion transformation with ffmpeg will not produce output files consistently.
 
+this package solves that problem by using ffprobe to detect certain attributes and then determines a better video encoding setting for ffmpeg resulting in more predictable output videos.
 
 ## requirements
 
  - install both ffmpeg and ffprob and have them accesable on the path
+ - run `npm i` to install this packages dependencies
+
+## usage
+
+
+```
+var ae = require('adaptive-encoding')
+ae.encode('output-name', ['avi', 'mpeg4'], 1024)
+```
