228202814fe7658121223c19024d49dbe464d697
samthomson <samt@samt.st>
2017-05-03 23:26:22 +0200

    bug fixes

--- index.js
+++ index.js
@@ -46,27 +46,27 @@ function pAVI (sInputFile, sOutputFilename) {
 function convertAll(sInputFile, sOutputFilename, aOutputFormats) {
 	// build up array of conversion promises
 	let aFormats = [];
-	aOutputFormats.forEach(value => {
+	aOutputFormats.forEach((value) => {
 		switch(value)
 		{
 			case 'mpeg4':
-				aFormats.push(pAVI(sInputFile, sOutputFilename))
+				aFormats.push(pMP4(sInputFile, sOutputFilename))
 				break;
 			case 'avi':
 				aFormats.push(pAVI(sInputFile, sOutputFilename))
 				break;
 			case 'webm':
-				aFormats.push(pAVI(sInputFile, sOutputFilename))
+				aFormats.push(pWEBM(sInputFile, sOutputFilename))
 				break;
 		}
 	})
 	// do all requested conversion
 	Promise.all(aFormats).then(() => {
-	    console.log('finished conevrting')
+	    console.log(`finished converting ${sInputFile}`)
 	});
 }
 
 exports.encode = function(sInputFile, sOutputFilename, aOutputFormats, iKilobytesPerSecond) {
 	console.log(`convert ${sInputFile} into ${sOutputFilename}`);
-	convertAll(sInputFile, sOutputFilename)
+	convertAll(sInputFile, sOutputFilename, aOutputFormats)
 }
